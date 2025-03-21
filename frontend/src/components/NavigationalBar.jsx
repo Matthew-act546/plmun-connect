2631e9f2aa3670665b0413c424c4c96b6ffe7835
matthew <matthewyt765@gmail.com>
2025-03-21 17:29:56 +0800

    fix

--- frontend/src/components/NavigationalBar.jsx
+++ frontend/src/components/NavigationalBar.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/images/logo.png';
+import logo from '../assets/images/Logo.png';
 import Button from 'react-bootstrap/Button';
 
 export default function NavigationalBar() {
